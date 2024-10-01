f1bd6f00075f025306087f2af518bdb721d4dca3
IsaacLorenc <107570569+IsaacLorenc@users.noreply.github.com>
2024-10-01 11:53:19 -0600

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env', // Transforms modern JS to be compatible with older environments
-    ['@babel/preset-react', { runtime: 'automatic' }] // Ensures JSX is compiled properly
-  ],
-  plugins: []
-};
