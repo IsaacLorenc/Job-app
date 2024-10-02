372643052287cc14f4644e70af2f0a33a370a01c
IsaacLorenc <107570569+IsaacLorenc@users.noreply.github.com>
2024-10-02 13:47:41 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,3 @@
-export default {
-  server: {
-    hmr: {
-      host: 'job-app-70wg.onrender.com',  // e.g., 'job-app-70wg.onrender.com'
-      protocol: 'wss',
-      port: 443,  // Standard WebSocket over HTTPS
-    }
-  },
+
 
 
