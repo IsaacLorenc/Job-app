cb76fe31ddedf17af7ef3950b5b72f03b0efbca2
IsaacLorenc <107570569+IsaacLorenc@users.noreply.github.com>
2024-10-05 12:33:25 -0600

    Update index.jsx

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:5173/api/users";
+			const url = "/api/users";
 			const { data: res } = await axios.post(url, data);
 			navigate("/login");
 			console.log(res.message);
