fdb07b7cab635995521d6098290d568a2df3071b
IsaacLorenc <107570569+IsaacLorenc@users.noreply.github.com>
2024-10-05 15:40:50 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,4 +27,5 @@ app.get('*', (req, res) => {
 });
 
 const port = process.env.PORT || 5173;
-app.listen(port, console.log(`Listening on port ${port}...`));
+// app.listen(port, console.log(`Listening on port ${port}...`));
+module.exports = app;
